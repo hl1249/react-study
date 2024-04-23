d2275b877a58ef1ad45a6e641d9923ad7340d0d4
1249412130@qq.com <1249412130@qq.com>
2024-04-24 00:04:07 +0800

    懒加载组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,43 @@
-import { Component } from 'react'
-import './App.css'
+import { NavLink } from "react-router-dom";
+import { Routes, Route, } from 'react-router-dom'
+import {lazy} from 'react'
+import { Suspense } from "react";
+// import ClassTemplate from "@/pages/Class";
 
-import Class_template from '@/pages/Class'
+const ClassTemplate =  lazy(() => import('@/pages/Class'));
+import UseEffectTemplate from "@/pages/UseEffectTemplate";
+function App() {
+  const routes = [
+    {
+      name: "类组件",
+      path: "/classTemplate",
+      element: <ClassTemplate />,
+    },
+    {
+      name: "useEffect使用",
+      path: "/useEffectTemplate",
+      element: <UseEffectTemplate />,
+    }
+  ]
 
-class App extends Component{
-  render(){
-    return(
-      <div className="App">
-        <h1>Hello, World!</h1>
-        <Class_template></Class_template>
-      </div>
-    )
-  }
+  return (
+    <div className="App">
+
+      <nav>
+        {routes.map((item, index) => {
+          return <NavLink key={index} to={item.path} className={({ isActive }) => isActive ? "nav-active" : void 0}>{item.name}</NavLink>
+        })}
+      </nav>
+
+      <Suspense>
+      <Routes>
+       
+        <Route path="/classTemplate" element={<ClassTemplate />} />
+        <Route path="/useEffectTemplate" element={<UseEffectTemplate />} />
+      </Routes>
+        </Suspense>
+    </div>
+  );
 }
 
 export default App
---
+++ src/pages/UseEffectTemplate.tsx
@@ -0,0 +1,9 @@
+function useEffectTemplate() {
+    return(
+        <div>
+            我是useEffectTemplate
+        </div>
+    )
+}
+
+export default useEffectTemplate
