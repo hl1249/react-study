90dad5420abc31054d2110252b14bcf4d82c87b4
1249412130@qq.com <1249412130@qq.com>
2024-04-25 01:11:43 +0800

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
 import { NavLink } from "react-router-dom";
 import { Routes, Route, } from 'react-router-dom'
-import {lazy} from 'react'
+import { lazy } from 'react'
 import { Suspense } from "react";
 // import ClassTemplate from "@/pages/Class";
 
-const ClassTemplate =  lazy(() => import('@/pages/Class'));
+// PS：使用懒加载路由需要用 Suspense包裹<Route>
+const ClassTemplate = lazy(() => import('@/pages/Class'));
 import UseEffectTemplate from "@/pages/UseEffectTemplate";
 function App() {
   const routes = [
     {
       name: "类组件",
       path: "/classTemplate",
-      element: <ClassTemplate />,
     },
     {
       name: "useEffect使用",
       path: "/useEffectTemplate",
-      element: <UseEffectTemplate />,
     }
   ]
 
@@ -30,12 +29,23 @@ function App() {
       </nav>
 
       <Suspense>
-      <Routes>
-       
-        <Route path="/classTemplate" element={<ClassTemplate />} />
-        <Route path="/useEffectTemplate" element={<UseEffectTemplate />} />
-      </Routes>
-        </Suspense>
+        <Routes>
+          {/* <Route path="/classTemplate" lazy={async () => {
+            const module =  await import('@/pages/Class');
+            const classTemplate = module.default;
+
+            return {
+              element:(
+                <div>我测法</div>
+              )
+            }
+          }}></Route> */}
+
+
+          <Route path="/classTemplate" element={<ClassTemplate />} />
+          <Route path="/useEffectTemplate" element={<UseEffectTemplate />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
