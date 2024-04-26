22fc1ec2c94c826e23ba30317ddb509b54063d52
hr1249412130 <1249412130@qq.com>
2024-04-26 18:01:13 +0800

    提交

--- src/pages/UseEffectTemplate.tsx
+++ src/pages/UseEffectTemplate.tsx
@@ -1,14 +1,17 @@
-import { useEffect, useState } from "react"
+import { createRef, useEffect, useState } from "react"
 
 function useEffectTemplate() {
 
-    const [name,setName] = useState('')
+    const [name,setName] = useState('张三')
 
+    const myRef = createRef() as React.MutableRefObject<HTMLDivElement>
     useEffect(() => {
-        console.log("useEffectTemplate")
+        console.log("useEffect",name)
+
+        console.log(myRef)
 
         return () => {
-            console.log("useEffectTemplate return")
+            console.log("return-useEffect",name)
         }
     })
 
@@ -17,6 +20,9 @@ function useEffectTemplate() {
     return(
         <div>
             我是useEffectTemplate
+            <button onClick={()=>setName(name+1)}>修改{name}</button>
+
+            <p ref={myRef}>我是p标签</p>
         </div>
     )
 }
